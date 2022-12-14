5be5a8c52850fef2e3756abed26daf19df520f72
Ramon de las Heras <rdelasheras@gmail.com>
2022-12-14 16:15:27 +0100

    Refactor

--- src/entities_levels.asm
+++ src/entities_levels.asm
@@ -1,8 +1,9 @@
 init_entities_data
-    dw entities_level_0, entities_level_1
-entities_level_0
+    dw entities_sea_0, entities_sea_1
+entities_sea_0
     db INDESTRUCTIBLE,INIT_TYPE_0,ENTITY_GEN_STOPWALK,15,20,15*8,20*8
     db DESTRUCTIBLE,INIT_TYPE_0,ENTITY_STOPWALK,15,20,15*8,20*8
     db EOF
-entities_level_1
+entities_sea_1
+    db DESTRUCTIBLE,INIT_TYPE_0,ENTITY_STOPWALK,20,5,20*8,5*8
     db EOF
--- src/game.asm
+++ src/game.asm
@@ -11,14 +11,6 @@ init_level_0
         
         call    init_entities_level
 
-;        call    get_next_empty_destructible_entity_ix
- ;       ld      [ix+OFFSET_TYPE], ENTITY_STOPWALK
-  ;      ld      [ix+OFFSET_STATE], 0
-   ;     ld      [ix+OFFSET_STATE_COUNTER], 0
-    ;    ld      [ix+OFFSET_X], 4*8
-     ;;   ld      [ix+OFFSET_Y], 8*8
-       ; ld      [ix+OFFSET_IS_VISIBLE], 1
-
         ret
 
 init_level_1
@@ -29,6 +21,8 @@ init_level_1
         ld      hl, SEA_SCREEN_1
         call    build_level
         call    reset_entities
+
+        call    init_entities_level
 
         ret
 
