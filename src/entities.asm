d2f4d6a7874ede30487178b8b0929d85cfa502a6
Ramon de las Heras <rdelasheras@gmail.com>
2023-02-02 17:55:52 +0100

    Gen_stopwalk with character number

--- src/entities.asm
+++ src/entities.asm
@@ -29,15 +29,22 @@ trate_gen_stopwalk
 
                 ; Generate a stopwalk entity
 
+        ld      a, [ix+OFFSET_CHARACTER_TYPE]
+
         push    ix
 
-        call    get_next_empty_destructible_entity_ix
-        ld      [ix+OFFSET_TYPE], ENTITY_STOPWALK
-        ld      [ix+OFFSET_STATE], 0
-        ld      [ix+OFFSET_STATE_COUNTER], 0
-        ld      [ix+OFFSET_X], 31*8
-        ld      [ix+OFFSET_Y], 8*8
-        ld      [ix+OFFSET_IS_VISIBLE], 1
+                push    af
+
+                        call    get_next_empty_destructible_entity_ix
+                        ld      [ix+OFFSET_TYPE], ENTITY_STOPWALK
+                        ld      [ix+OFFSET_STATE], 0
+                        ld      [ix+OFFSET_STATE_COUNTER], 0
+                        ld      [ix+OFFSET_X], 31*8
+                        ld      [ix+OFFSET_Y], 8*8
+                        ld      [ix+OFFSET_IS_VISIBLE], 1
+                pop     af
+
+                ld      [ix+OFFSET_CHARACTER_TYPE], a
 
         pop     ix
 
